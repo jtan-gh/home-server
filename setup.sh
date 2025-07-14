3d5978eebc93a449b7e1a38f174d7e5a9868f5fe
jtan-gh <jtantan99@gmail.com>
2025-07-14 01:14:18 -0700

    adding sudo

--- setup.sh
+++ setup.sh
@@ -12,7 +12,7 @@ DEPLOYMENT_FILE="/cloudflare/deployment.yaml"
 DEBUG_POD_FILE="debug-pod.yaml"
 
 echo "Creating namespace 'cloudflared' if not exists..."
-kubectl get ns cloudflared >/dev/null 2>&1 || kubectl create ns cloudflared
+sudo kubectl get ns cloudflared >/dev/null 2>&1 || sudo kubectl create ns cloudflared
 
 echo "Encoding tunnel.json..."
 ENCODED_TUNNEL_JSON=$(base64 -w 0 "$TUNNEL_JSON_PATH")
@@ -31,18 +31,18 @@ data:
 EOF
 
 echo "Applying Secret..."
-kubectl apply -f "$SECRET_FILE"
+sudo kubectl apply -f "$SECRET_FILE"
 
 echo "Replacing tunnelID inside cloudflared ConfigMap..."
 sed -i "s/^[[:space:]]*tunnel: .*/    tunnel: $TUNNEL_ID/" "$CONFIG_MAP_FILE"
 
 echo "Applying ConfigMap..."
-kubectl apply -f "$CONFIG_MAP_FILE"
+sudo kubectl apply -f "$CONFIG_MAP_FILE"
 
 echo "Applying Deployment..."
-kubectl apply -f "$DEPLOYMENT_FILE"
+sudo kubectl apply -f "$DEPLOYMENT_FILE"
 
 echo "Creating debug pod! You can optionally run:"
-kubectl apply -f $DEBUG_POD_FILE
+sudo kubectl apply -f $DEBUG_POD_FILE
 echo "To connect to debug pod with sh use..."
 echo "kubectl exec -n cloudflared -it debug-config-check -- sh"
